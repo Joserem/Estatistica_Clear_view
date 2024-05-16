f59291c00cd19cdd48d757353c9980e3c61436ef
Joserem <jrsliva@outlook.com>
2024-05-16 11:04:52 -0300

    melhorando o grafico agora aceitando qualquer arquivo excel e ajeitando a imagem do home

--- criacao_excel.py
+++ criacao_excel.py
@@ -10,11 +10,20 @@ df = pd.DataFrame({
     'Satisfacao': np.random.randint(1, 10, size=100)
 })
 
-# Lista de categorias de produtos fictícios
-categorias_produtos = ['Eletrônicos', 'Roupas', 'Alimentos', 'Livros', 'Móveis', 'Jogos', 'Esportes', 'Jardinagem']
+# Lista de categorias de produtos expandida
+categorias_expandidas = [
+    'Eletrônicos', 'Roupas', 'Alimentos', 'Livros', 'Móveis', 'Jogos', 'Esportes', 'Jardinagem',
+    'Bebidas', 'Frutas', 'Legumes', 'Carnes', 'Peixes', 'Laticínios', 'Padaria', 'Congelados',
+    'Doces', 'Snacks', 'Cereais', 'Condimentos', 'Especiarias', 'Grãos', 'Massas', 'Sopas',
+    'Conservas', 'Biscoitos', 'Sorvetes', 'Café', 'Chás', 'Sucos', 'Águas', 'Refrigerantes',
+    'Artigos de Festa', 'Utensílios de Cozinha', 'Limpeza', 'Higiene Pessoal', 'Perfumaria',
+    'Maquiagem', 'Farmácia', 'Pet Shop', 'Brinquedos', 'Papelaria', 'Eletrodomésticos',
+    'Informática', 'Celulares', 'Fotografia', 'Decoração', 'Cama, Mesa e Banho', 'Ferramentas',
+    'Automotivos', 'Construção', 'Jardinagem Avançada'
+]
 
-# Adicionando a coluna 'Produtos' ao DataFrame
-df['Produtos'] = [random.choice(categorias_produtos) for _ in range(len(df))]
+# Atualizando a coluna 'Produtos' com a nova lista de categorias
+df['Produtos'] = [random.choice(categorias_expandidas) for _ in range(len(df))]
 
 # Salvando o DataFrame atualizado como um arquivo Excel
 df.to_excel('dados_empresa.xlsx', index=False)
